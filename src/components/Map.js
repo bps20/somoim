18ea9bbda667dcf2f501927f52f58b3f40c37c48
KJH <bestpas20@naver.com>
2022-11-22 05:58:59 +0900

    닫기 까지 만듬

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,37 +1,77 @@
 /* global kakao */
 import React, { useEffect } from "react";
+import "components/Map.css";
 
 const { kakao } = window;
-
+function closeOverlay(overlay) {
+    return function () {
+        overlay.setMap(null);
+    };
+}
 const Map = () => {
     let map;
-    const initMapLat = 36.35133;
-    const initMapLng = 127.734086;
+    const initMapLat = 36.35133, initMapLng = 127.734086;
 
     useEffect(() => {
         let container = document.getElementById("map");
         let options = {
             center: new window.kakao.maps.LatLng(initMapLat, initMapLng),
-            draggable: false,
+            draggable: true,
             level: 12,
         };
 
         map = new window.kakao.maps.Map(container, options); //지도 생성
+        var zoomControl = new kakao.maps.ZoomControl();
+        map.addControl(zoomControl, kakao.maps.ControlPosition.RIGHT);
 
         var markerInfo = [
             {
-                title: '맛집 테스트1',
+                content: '<div class="wrap">' +
+                    '    <div class="info">' +
+                    '        <div class="title">' +
+                    '            맛집 제1번' +
+                    // '            <div class="close" onclick={closeOverlay(overlay)} title="닫기"></div>' +
+                    '        </div>' +
+                    '        <div class="body">' +
+                    '            <div class="img">' +
+                    '                <img src="https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/marker_red.png" width="73" height="70">' +
+                    '           </div>' +
+                    '            <div class="desc">' +
+                    '                <div class="ellipsis">서울특별시 왕십리로 222</div>' +
+                    '                <div class="jibun ellipsis">(우) 11111 (지번) 사근동 222</div>' +
+                    '                <div><a href="https://www.hanyang.ac.kr" target="_blank" class="link">홈페이지</a></div>' +
+                    '            </div>' +
+                    '        </div>' +
+                    '    </div>' +
+                    '</div>',
                 latlng: new kakao.maps.LatLng(37.54699, 127.09598),
-                mark: true
+                ismark: false
             },
             {
-                title: '생태연못',
-                latlng: new kakao.maps.LatLng(33.450936, 126.569477),
-                mark: true
+                content: '<div class="wrap">' +
+                    '    <div class="info">' +
+                    '        <div class="title">' +
+                    '            카카오 스페이스닷원' +
+                    //'            <div class="close" onclick="closeOverlay()" title="닫기"></div>' +
+                    '        </div>' +
+                    '        <div class="body">' +
+                    '            <div class="img">' +
+                    '                <img src="https://cfile181.uf.daum.net/image/250649365602043421936D" width="73" height="70">' +
+                    '           </div>' +
+                    '            <div class="desc">' +
+                    '                <div class="ellipsis">제주특별자치도 제주시 첨단로 242</div>' +
+                    '                <div class="jibun ellipsis">(우) 63309 (지번) 영평동 2181</div>' +
+                    '                <div><a href="https://www.kakaocorp.com/main" target="_blank" class="link">홈페이지</a></div>' +
+                    '            </div>' +
+                    '        </div>' +
+                    '    </div>' +
+                    '</div>',
+                latlng: new kakao.maps.LatLng(35.450936, 126.569477),
+                ismark: false
             }
         ];
 
-        var imageSrc = 'https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/marker_red.png', // 마커이미지의 주소입니다    
+        var imageSrc = 'https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/marker_red.png',
             imageSize = new kakao.maps.Size(64, 69),
             imageOption = { offset: new kakao.maps.Point(27, 69) };
         var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imageOption);
@@ -39,51 +79,65 @@ const Map = () => {
         // 마커를 생성합니다
         for (var i = 0; i < markerInfo.length; i++) {
             var marker = new kakao.maps.Marker({
+                map: map,
                 position: markerInfo[i].latlng,
-                image: markerImage // 마커이미지 설정 
+                image: markerImage
             });
-            marker.setMap(map); //마커표시
+            //marker.setMap(map); //마커표시
+
+            var overlay = new kakao.maps.CustomOverlay({
+                map: map,
+                clickable: true,
+                content: markerInfo[i].content,
+                position: marker.getPosition(),
+                xAnchor: 0,
+                yAnchor: 2
+            });
+
+            var content = document.createElement("div");
+            content.innerHTML = markerInfo[i].content;
+
+            var closeBtn = document.createElement("button");
+            closeBtn.innerHTML = '닫기';
+            closeBtn.onclick = closeOverlay(overlay);
+
+            content.appendChild(closeBtn);
+            overlay.setContent(content);
+
+            overlay.setMap(null);
+            kakao.maps.event.addListener(marker, 'click', clickListener(map, overlay));
+
         }
 
-        
+        function clickListener(mapÍ, overlay) {
+            return function () {
+                overlay.setMap(map);
+            };
+        }
+        function closeOverlay(overlay) {
+            return function () {
+                overlay.setMap(null);
+            };
+        }
 
-        var iwContent = '<div style="padding:10px;">맛집 이름</div><br><a href="https://map.kakao.com/link/map/클릭시이렇게보임,33.450701,126.570667" style="padding:5px; color:blue" target="_blank">지도 크게보기</a>',
-            iwPosition = new kakao.maps.LatLng(37.54699, 127.09598),
-            iwRemoveable = true; // removeable 속성을 ture 로 설정하면 인포윈도우를 닫을 수 있는 x버튼이 표시됩니다
+        function zoomIn() {
+            map.setLevel(map.getLevel() - 1);
+        }
 
-        var infowindow = new kakao.maps.InfoWindow({
-            map: map,
-            position: iwPosition,
-            content: iwContent,
-            removable: iwRemoveable
-        });
-        // 마커 위에 인포윈도우를 표시합니다. 두번째 파라미터인 marker를 넣어주지 않으면 지도 위에 표시됩니다
-        infowindow.open(map, marker);
+        // 지도 확대, 축소 컨트롤에서 축소 버튼을 누르면 호출되어 지도를 확대하는 함수입니다
+        function zoomOut() {
+            map.setLevel(map.getLevel() + 1);
+        }
 
-        //클릭시 인포윈도우 표시.
-        kakao.maps.event.addListener(marker, 'click', function () {
-            infowindow.open(map, marker);
-        });
-        // 마커에 마우스오버 이벤트를 등록합니다
-        kakao.maps.event.addListener(marker, 'mouseover', function () {
-            // 마커에 마우스오버 이벤트가 발생하면 인포윈도우를 마커위에 표시합니다
-            infowindow.open(map, marker);
-        });
 
-        // 마커에 마우스아웃 이벤트를 등록합니다
-        kakao.maps.event.addListener(marker, 'mouseout', function () {
-            // 마커에 마우스아웃 이벤트가 발생하면 인포윈도우를 제거합니다
-            infowindow.close();
-        });
 
     }, []);
 
-    const onMoveToggleClick = () => map.setDraggable((prev) => !prev); //지도이동 끄기
+
 
     return (
         <>
             <div id="map" style={{ width: "700px", height: "900px" }}></div>
-            <button onClick={onMoveToggleClick}>이동방지 풀기</button>
         </>
     );
 };
